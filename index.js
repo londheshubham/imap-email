a744e739acce381ab8162fcd57d19baff99215b8
londheshubham <londheshubham68@gmail.com>
2022-04-20 13:44:09 +0530

    live email monitoring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Imap = require('imap');
 require('dotenv').config();
-const {simpleParser} = require('mailparser');
+const { simpleParser } = require('mailparser');
 const imapConfig = {
   user: 'londheshubham68@gmail.com',
   password: process.env.password,
@@ -13,57 +13,85 @@ const imapConfig = {
 let emailData = [];
 
 const getEmails = () => {
-    try {
-      const imap = new Imap(imapConfig);
-      imap.once('ready', () => {
-        imap.openBox('INBOX', false, () => {
-          imap.search(['UNSEEN', ['SINCE', new Date('2022-04-11')]], (err, results) => {
-            const f = imap.fetch(results, {bodies: ''});
-            f.on('message', msg => {
-              msg.on('body', stream => {
-                simpleParser(stream, async (err, parsed) => {
-                    saveData(parsed)
-                });
+  try {
+    const imap = new Imap(imapConfig);
+    imap.once('ready', () => {
+      imap.openBox('INBOX', false, () => {
+        imap.search(['ALL', ['SINCE', new Date('2022-04-11')]], (err, results) => {
+          const f = imap.fetch(results, { bodies: '' });
+          f.on('message', msg => {
+            msg.on('body', stream => {
+              simpleParser(stream, async (err, parsed) => {
+                saveData(parsed)
               });
             });
-            f.once('error', ex => {
-              return Promise.reject(ex);
-            });
-            f.once('end', () => {
-              console.log('Done fetching all messages!');
-              imap.end();
-            });
+          });
+          f.once('error', ex => {
+            return Promise.reject(ex);
+          });
+          f.once('end', () => {
+            console.log('Done fetching all messages!');
+            imap.end();
           });
         });
-      });  
-      imap.once('error', err => {
-        console.log(err);
       });
-  
-      imap.once('end', () => {
-        console.log('Connection ended');
-        writeDataToJson()
-      });
-  
-      imap.connect();
-    } catch (ex) {
-      console.log('an error occurred');
-    }
+    });
+    imap.once('error', err => {
+      console.log(err);
+    });
+
+    imap.once('end', () => {
+      console.log('Connection ended');
+      writeDataToJson()
+    });
+
+    imap.connect();
+  } catch (ex) {
+    console.log('an error occurred');
+  }
+};
+
+
+const checkIncomingOrDeletedEmails = () => {
+  try {
+    const imap = new Imap(imapConfig);
+    imap.on('ready', () => {
+      imap.openBox('INBOX', true, (err, box) => {
+        if (err) console.log(err)
+        else console.log('inbox is being monitored')
+        imap.on('mail', function () {
+          console.log('you have received a new email');
+        });
+        imap.on('expunge', function () {
+          console.log('an email has been deleted from the inbox');
+        })
+      })
+    })
+    imap.once('error', err => {
+      console.log(err);
+    });
+    imap.connect();
+  } catch (ex) {
+    console.log('an error occurred');
+  }
+};
+
+const saveData = (data) => {
+  const obj = {
+    subject: data.subject,
+    date: data.date,
+    text: data.text,
+    textAsHtml: data.textAsHtml,
   };
-  
-  const saveData = (data) => {
-    const obj = {
-        subject: data.subject,
-        date: data.date,
-        text: data.text,
-        textAsHtml: data.textAsHtml,
-    };
-    emailData.push(obj);
-  }
+  emailData.push(obj);
+}
 
-  const writeDataToJson = () => {
-    const jsonToBeWritten = JSON.stringify(emailData, null, 2);
-    fs.writeFileSync('data.json', jsonToBeWritten);
-  }
+const writeDataToJson = () => {
+  const jsonToBeWritten = JSON.stringify(emailData, null, 2);
+  fs.writeFileSync('data.json', jsonToBeWritten);
+}
 
-  getEmails();
+getEmails();
+
+checkIncomingOrDeletedEmails();
+
